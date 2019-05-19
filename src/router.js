8be4a5a0a7c34ab95a8d56f8d071555d2ec1273f
Actedlasmiranda <mariofilipovic1997@gmail.com>
2019-05-19 14:41:04 +0200

    aufräumen

--- src/router.js
+++ src/router.js
@@ -34,14 +34,6 @@ export default new Router({
           path: '/konkurrenz',
           name: 'konkurrenz',
           component: Konkurrenz
-      },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+      }
   ]
 })

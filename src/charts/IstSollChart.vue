a75edb34522da96f4225bb1f184bae2707866cc3
Mustapha Ceesay <mustapha.ceesay@gmx.ch>
2019-05-28 17:39:29 +0200

    not totally stable

--- src/charts/IstSollChart.vue
+++ src/charts/IstSollChart.vue
@@ -1,37 +1,60 @@
 <script>
-    import { Doughnut } from "vue-chartjs";
+	import {Doughnut} from "vue-chartjs";
+	import axios from "axios";
 
-    export default {
-        extends: Doughnut,
-        data() {
-            return {
-                datacollection: {
-                    labels: ["Ist", "Soll"],
-                    datasets: [
-                        {
-                            label: "Marktanteil in %",
-                            backgroundColor: [
-                                "#F4BD59",
-                                "#EF7970"
-                            ],
-                            data: [40, 20]
-                        }
-                    ]
-                },
-                options: {
-                    rotation: 1.14 * Math.PI,
-                    circumference: 0.7*Math.PI,
-                    cutoutPercentage:65,
-                    legend: {
-                        display: true
-                    },
-                    responsive: false,
-                    maintainAspectRatio: false
-                }
-            };
-        },
-        mounted() {
-            this.renderChart(this.datacollection, this.options);
-        }
-    };
+	export default {
+		extends: Doughnut,
+		data() {
+			return {
+				datacollection: {
+					labels: ["Ist", "Verbleibend"],
+					datasets: [
+						{
+							label: "Marktanteil in %",
+							backgroundColor: [
+								"#F4BD59",
+								"#EF7970"
+							],
+							data: [40, 20]
+						}
+					]
+				},
+				options: {
+					rotation: 1.14 * Math.PI,
+					circumference: 0.7 * Math.PI,
+					cutoutPercentage: 65,
+					legend: {
+						display: true
+					},
+					responsive: false,
+					maintainAspectRatio: false
+				}
+			};
+		},
+		methods: {
+			renderDoghnutChart: function() {
+				this.renderChart(this.datacollection, this.options);
+			},
+			getApiData: function () {
+				axios
+					.get("http://localhost:8080/infmapi/v1/revenue")
+					.then(res => {
+						this.datacollection.datasets[0].data[0] = res.data;
+						console.log(this.datacollection.datasets[0].data);
+					})
+					.catch(err => console.log(err));
+				axios
+					.get("http://localhost:8080/infmapi/v1/objectives/Umsatz")
+					.then(res => {
+						this.datacollection.datasets[0].data[1] = (res.data[2019][0].objective + res.data[2019][1].objective + res.data[2019][2].objective + res.data[2019][3].objective) - this.datacollection.datasets[0].data[0];
+					    console.log(this.datacollection.datasets[0].data);
+					    this.renderDoghnutChart();
+					})
+					.catch(err => console.log(err));
+			}
+		},
+		mounted() {
+			this.getApiData();
+		}
+	};
 </script>
